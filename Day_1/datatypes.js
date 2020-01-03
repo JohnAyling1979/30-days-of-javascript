c2e96b0bdba678e05206fc75ac73b0cb6ee5b523
John Ayling <john.ayling@digintent.com>
2020-01-03 17:18:14 -0600

    Day 1

---
+++ Day_1/datatypes.js
@@ -0,0 +1,7 @@
+function getTypeof() {
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+    console.log(typeof d);
+    console.log(typeof e);
+}
---
+++ Day_1/variable.js
@@ -0,0 +1,5 @@
+const a = 'This is a string';
+const b = true;
+const c = undefined;
+const d = null;
+const e = 9000;
